30a7d7293634f83f53ac9e44939df908362cb3c5
Chris <christopher.oconnor@santismail.ch>
2021-02-02 10:29:29 +0100

    Update ArduinoM424.ino

--- ArduinoM424/ArduinoM424.ino
+++ ArduinoM424/ArduinoM424.ino
@@ -1,101 +1,112 @@
+/**
+ *  BBBaden Modul 424
+ *	
+ *	Author: O'Connor Chris In18z
+ *  Date:	02/07/2021
+ *
+ */
 
-/* Width of data (how many ext lines).
-*/
-#define DATA_WIDTH   16
+// Shift Register Input (165)
+int ploadPin        = 4; 	// Connects to Parallel load pin
+int clockEnablePin  = 5; 	// Connects to Clock Enable pin
+int dataPin         = 6; 	// Connects to the Q7 pin
+int clockPin        = 7; 	// Connects to the Clock pin
 
-#define BYTES_VAL_T unsigned int
+// Shift Register Output (595)
+int latchPinOUT =  8;      	//Pin connected to ST_CP
+int clockPinOUT = 12;      	//Pin connected to SH_CP
+int dataPinOUT  = 11;      	//Pin connected to DS
 
-
-// 165
-int ploadPin        = 4;  // Connects to Parallel load pin the 165
-int clockEnablePin  = 5;  // Connects to Clock Enable pin the 165
-int dataPin         = 6; // Connects to the Q7 pin the 165
-int clockPin        = 7; // Connects to the Clock pin the 165
-
-// 595
-int latchPinOUT =  8;      //Pin connected to ST_CP
-int clockPinOUT = 12;      //Pin connected to SH_CP
-int dataPinOUT  = 11;      //Pin connected to DS
-
+// Queue and size
+int qSize   = 6;
 int queue[] = {0, 0, 0, 0, 0, 0};
-int qSize = 6;
+
+// Timer Interrupt
 unsigned long targetTime = millis() + 10000;
 
+// Configurations for LEDs
 int light_configs [7][3] = {{0, 255, 240}, {34, 221, 240}, {137, 118, 240}, {4, 251, 180}, {16, 239, 105}, {64, 191, 225}, {129, 126, 150}};
 
-
+#define BYTES_VAL_T unsigned int
 BYTES_VAL_T pinValues;
 BYTES_VAL_T oldPinValues;
 
-/* This function is essentially a "shift-in" routine reading the
+/* 
+ * This function is essentially a "shift-in" routine reading the
  * serial Data from the shift register chips and representing
- * the state of those pins in an unsigned integer (or long).
-*/
+ * the state of those pins in an unsigned integer.
+ * Source: https://playground.arduino.cc/Code/ShiftRegSN74HC165N/
+ */
 BYTES_VAL_T read_shift_regs()
 {
-    long bitVal;
-    BYTES_VAL_T bytesVal = 0;
+	long bitVal;
+	BYTES_VAL_T bytesVal = 0;
 
-    /* Trigger a parallel Load to latch the state of the data lines,
-    */
-    digitalWrite(clockEnablePin, HIGH);
-    digitalWrite(ploadPin, LOW);
-    delayMicroseconds(5);
-    digitalWrite(ploadPin, HIGH);
-    digitalWrite(clockEnablePin, LOW);
+	// Trigger a parallel Load to latch the state of the data lines,
+	digitalWrite(clockEnablePin, HIGH);
+	digitalWrite(ploadPin, LOW);
+	delayMicroseconds(5);
+	digitalWrite(ploadPin, HIGH);
+	digitalWrite(clockEnablePin, LOW);
 
-    /* Loop to read each bit value from the serial out line
-     * of the SN74HC165N.
-    */
-    for(int i = 0; i < DATA_WIDTH; i++)
-    {
-        bitVal = digitalRead(dataPin);
+	/* Loop to read each bit value from the serial out line
+	* of the SN74HC165N.
+	*/
+	for(int i = 0; i < 16; i++)
+	{
+		bitVal = digitalRead(dataPin);
 
-        /* Set the corresponding bit in bytesVal.
-        */
-        bytesVal |= (bitVal << ((DATA_WIDTH-1) - i));
+		// Set the corresponding bit in bytesVal.
+		bytesVal |= (bitVal << ((16-1) - i));
 
-        /* Pulse the Clock (rising edge shifts the next bit).
-        */
-        digitalWrite(clockPin, HIGH);
-        delayMicroseconds(5);
-        digitalWrite(clockPin, LOW);
-    }
+		// Pulse the Clock (rising edge shifts the next bit).
+		digitalWrite(clockPin, HIGH);
+		delayMicroseconds(5);
+		digitalWrite(clockPin, LOW);
+	}
 
-    return(bytesVal);
+	return(bytesVal);
 }
 
-/* Dump the list of zones along with their current status.
-*/
+/* 
+ * Evaluate Input Pin Values.
+ */
 void eval_pin_values()
 {
-    for(int i = 0; i < DATA_WIDTH; i++)
-    {
-        if((pinValues >> i) & 1 ) {
-          Serial.print("INPUT: ");
-          Serial.println(i);
-          mapInputToQueue(i);
-        }
-        
-    }
+	for(int i = 0; i < 16; i++)
+	{
+		if((pinValues >> i) & 1 ) {
+		  Serial.print("INPUT: ");
+		  Serial.println(i);
+		  mapInputToQueue(i);
+		}
 
-    Serial.print("\r\n");
+	}
+
+	Serial.print("\r\n");
 }
 
+/*
+ * Display Configuration on LEDs.
+ * Source: https://www.arduino.cc/en/Tutorial/Foundations/ShiftOut
+ */
 void display_led(int arrIndex)
 {
-  Serial.print("Showing Config: ");
-  Serial.println(arrIndex);
-  
-  digitalWrite(latchPinOUT, LOW);
-  // shift out the bits:
-  shiftOut(dataPinOUT, clockPinOUT, MSBFIRST,  light_configs[arrIndex][2]); // REG 3.
-  shiftOut(dataPinOUT, clockPinOUT, MSBFIRST,  light_configs[arrIndex][1]); // REG 2.
-  shiftOut(dataPinOUT, clockPinOUT, MSBFIRST,  light_configs[arrIndex][0]); // REG 1.  
-  digitalWrite(latchPinOUT, HIGH);
-  
+	Serial.print("Showing Config: ");
+	Serial.println(arrIndex);
+
+	digitalWrite(latchPinOUT, LOW);
+	// shift out the bits:
+	shiftOut(dataPinOUT, clockPinOUT, MSBFIRST,  light_configs[arrIndex][2]); // REG 3.
+	shiftOut(dataPinOUT, clockPinOUT, MSBFIRST,  light_configs[arrIndex][1]); // REG 2.
+	shiftOut(dataPinOUT, clockPinOUT, MSBFIRST,  light_configs[arrIndex][0]); // REG 1.  
+	digitalWrite(latchPinOUT, HIGH);
+
 }
 
+/*
+ * Map Input Sensor ID to Configuration ID.
+ */
 void mapInputToQueue(int i)
 {
 	switch (i) {
@@ -120,97 +131,94 @@ void mapInputToQueue(int i)
 
 void addQ(int val)
 {
-  Serial.print("Q: ");
-  Serial.println(val);
-  for(int i = 0; i < qSize; i++) {
-    
-	// Exit if already in Q exists
-    if(queue[i] == val) { 
-      break; 
-    }
-    
-	// ADD
-    if(queue[i] == 0) {
-      queue[i] = val;
-      break;
-    }
-    
-  }
+	Serial.print("Q: ");
+	Serial.println(val);
+	for(int i = 0; i < qSize; i++) {
+
+		// Exit if already in Q exists
+		if(queue[i] == val) { 
+			break; 
+		}
+
+		// ADD
+		if(queue[i] == 0) {
+			queue[i] = val;
+			break;
+		}
+
+	}
   
 }
 
 int popQ()
 {
-  int val = queue[0];
-  
-  for(int i = 0; i < qSize; i++) {
-    
-    if(i == qSize - 1) {
-     queue[qSize - 1] = 0;
-    } else {
-      queue[i] = queue[i + 1];
-    }
-    
-  }
-  
-  return val;
-  
+	int val = queue[0];
+
+	for(int i = 0; i < qSize; i++) {
+
+		if(i == qSize - 1) {
+			queue[qSize - 1] = 0;
+		} else {
+			queue[i] = queue[i + 1];
+		}
+
+	}
+
+	return val;
 }
 
 void printQ()
 {
-  Serial.print("Current Queue [");
-  for(int i = 0; i < qSize; i++) {
-   Serial.print(queue[i]);     
-  }
-  Serial.println("}"); 
+	Serial.print("Current Queue [");
+	for(int i = 0; i < qSize; i++) {
+		Serial.print(queue[i]);     
+	}
+	Serial.println("}"); 
 }
 
 
 
 void setup()
 {
-    Serial.begin(9600);
+	Serial.begin(9600);
 
-    /* Initialize our digital pins...
-    */
-    pinMode(latchPinOUT, OUTPUT);
-    pinMode(clockPinOUT, OUTPUT);
-    pinMode(dataPinOUT , OUTPUT);
-      
-    pinMode(ploadPin, OUTPUT);
-    pinMode(clockEnablePin, OUTPUT);
-    pinMode(clockPin, OUTPUT);
-    pinMode(dataPin, INPUT);
+	// Initialize pins...
+	pinMode(latchPinOUT, OUTPUT);
+	pinMode(clockPinOUT, OUTPUT);
+	pinMode(dataPinOUT , OUTPUT);
+	  
+	pinMode(ploadPin, OUTPUT);
+	pinMode(clockEnablePin, OUTPUT);
+	pinMode(clockPin, OUTPUT);
+	pinMode(dataPin, INPUT);
 
-    digitalWrite(clockPin, LOW);
-    digitalWrite(ploadPin, HIGH);
+	digitalWrite(clockPin, LOW);
+	digitalWrite(ploadPin, HIGH);
 
-    /* Read in and display the pin states at startup.
-    */
-    pinValues = read_shift_regs();
-    oldPinValues = pinValues;
-	  display_led(0);
+	// Read Shift Register and Display Config 0 (default).
+	pinValues    = read_shift_regs();
+	oldPinValues = pinValues;
+	display_led(0);
 }
 
+/*
+ * MAIN
+ */
 void loop()
 {
+	// Timer Interrupt - Display 1. config from Queue
+	if(millis() > targetTime) {
+		display_led(popQ());    
+		targetTime = millis() + 10000;
+		printQ();
+	}
 
-  if(millis() > targetTime) {
-    
-	  display_led(popQ());    
-    targetTime = millis() + 10000;
-    printQ();
-  }
-  
-  
-    pinValues = read_shift_regs();
-	
+	pinValues = read_shift_regs();
+
 	if(pinValues != oldPinValues) {
-		
 		eval_pin_values();
 		oldPinValues = pinValues;
 	}
-    
-    delay(1000);
+
+	delay(500);
 }
